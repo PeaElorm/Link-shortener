1a28ce1cb96bcda09a6756a9ed0ca11f487d238e
https://github.com/PeaElorm/test_demo.git <elormavugla7@gmail.com>
2023-07-06 17:16:03 +0000

    shorten and copy link functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
 import { useState } from "react"
+import {CopyToClipboard} from 'react-copy-to-clipboard'
 import './App.css'
 
 function App() {
   const [url, setUrl] = useState()
   const [shortendUrl, setShortendUrl] = useState('');
+
+  const shortenUrl = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        `https://api.shrtco.de/v2/shorten?url=${url}`
+      )
+      const data = await response.json()
+      setShortendUrl(data.result.full_short_link);
+    } catch (e) {
+      alert(e);
+    }
+  }
+
   return (
     <div className="app">
       <div className="shortener">
         <h2>LinkTrim: Your Shortcut to sharing.</h2>
         {/* input field */}
-        <form>
+        <form onSubmit={shortenUrl}>
           <input
             placeholder="Enter URL to shorten"
             value={url}
-            onChange={(e) => setDefaultResultOrder(e.target.value)}
+            onChange={(e) => setUrl(e.target.value)}
           />
           <button>shorten</button>
         </form>
+
+        {/* section to view shortened URLS */}
+        {shortendUrl &&
+          <div className="shortener__viewShot">
+            {shortendUrl}
+            <CopyToClipboard text={shortendUrl}>
+              <button onClick={() => alert ("URL copied to clipboard")}>copy</button>
+            </CopyToClipboard>
+            </div>
+        }
       </div>
     </div>
+      
   );
 }
 
